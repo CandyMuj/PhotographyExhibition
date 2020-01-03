bf360c966d54dd8fa1f245f3d8c0c29a621b5e5b
MuJ <jx003js@gmail.com>
2020-01-03 17:56:59 +0800

    update db

---
+++ code/APIServer/src/main/java/com/cc/pic/api/enumc/StatusCode.java
@@ -0,0 +1,15 @@
+package com.cc.pic.api.enumc;
+
+/**
+ * @ProJectName APIServer
+ * @FileName ErrorCode
+ * @Description
+ * @Author CandyMuj
+ * @Date 2019/12/24 17:21
+ * @Version 1.0
+ */
+public class ErrorCode {
+
+    private static final int success = 1;
+
+}
--- db/photography.sql
+++ db/photography.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 20/12/2019 15:12:51
+ Date: 03/01/2020 17:56:14
 */
 
 SET NAMES utf8mb4;
@@ -68,7 +68,17 @@ CREATE TABLE `category`  (
   `cname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '名称',
   `orderIndex` int(11) NULL DEFAULT NULL COMMENT '排序 - 越大排序越前',
   PRIMARY KEY (`categoryId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for categoryrealation
+-- ----------------------------
+DROP TABLE IF EXISTS `categoryrealation`;
+CREATE TABLE `categoryrealation`  (
+  `beanType` tinyint(4) NULL DEFAULT NULL COMMENT '关联实体类型，建议使用枚举，因为这个属于内部东西，不适合开放配置，也没必要，配置写起来也麻烦',
+  `beanId` int(11) NULL DEFAULT NULL COMMENT '实体id',
+  `categoryId` int(11) NULL DEFAULT NULL COMMENT '分类id'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '分类和实体关联表，适用于一个东西属于多个分类或标签\r\n如果一个东西只有一个分类或标签，那么直接在业务表中弄一个字段即可，不用这么麻烦\r\n如果那个分类只有一级那也没必要做关联表\r\n当然关联表是万能的，无论什么情况都可用，只是某些情况下可以不用这么麻烦' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for customer
@@ -88,11 +98,6 @@ CREATE TABLE `customer`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of customer
--- ----------------------------
-INSERT INTO `customer` VALUES (1, 'candy', '$2a$10$zvY8kkke1tNa6NZmJBgwlOp59CDMwnux505DPhlE/I.yDVNSlxodK', '七月', NULL, 3, 1, 1, '2019-12-18 16:32:40');
-
--- ----------------------------
 -- Table structure for dict
 -- ----------------------------
 DROP TABLE IF EXISTS `dict`;
@@ -107,7 +112,7 @@ CREATE TABLE `dict`  (
   `limit` int(2) NULL DEFAULT NULL COMMENT '自身限制 默认无限制（一般是限制最顶级的父级的权限）',
   `childrenLimit` int(2) NULL DEFAULT NULL COMMENT '子级限制',
   PRIMARY KEY (`dictId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for footerinfo
@@ -119,7 +124,7 @@ CREATE TABLE `footerinfo`  (
   `content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '文本 或者url',
   `enable` tinyint(4) NULL DEFAULT NULL COMMENT '1可用 0不可用',
   PRIMARY KEY (`footerInfoId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for homemodel
@@ -134,6 +139,18 @@ CREATE TABLE `homemodel`  (
   `orderIndex` int(11) NULL DEFAULT NULL COMMENT '排序 越大越靠前',
   `modelType` tinyint(4) NULL DEFAULT NULL COMMENT '模块类型 单行轮播图 多行展示 about 文章',
   PRIMARY KEY (`homeModelId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for photoalbum
+-- ----------------------------
+DROP TABLE IF EXISTS `photoalbum`;
+CREATE TABLE `photoalbum`  (
+  `photoAlbumId` int(11) NOT NULL AUTO_INCREMENT COMMENT '相册',
+  `albumName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '相册名',
+  `albumDesc` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '相册描述',
+  `addTime` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`photoAlbumId`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -142,8 +159,13 @@ CREATE TABLE `homemodel`  (
 DROP TABLE IF EXISTS `picturelibrary`;
 CREATE TABLE `picturelibrary`  (
   `pictureLibraryId` int(11) NOT NULL AUTO_INCREMENT COMMENT '图片库',
+  `photoAlbumId` int(11) NULL DEFAULT NULL COMMENT '相册id',
+  `picName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片名称',
+  `picDesc` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片描述',
+  `picUri` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图片链接',
+  `addTime` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`pictureLibraryId`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for poster
@@ -174,7 +196,7 @@ CREATE TABLE `sysconfig`  (
   `sysvalue` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `desc` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`sysId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for thirdbind
