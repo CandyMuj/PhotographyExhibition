89290a273470f26e7736c29ea63c73ac029770ef
MuJ <jx003js@gmail.com>
2020-01-09 15:23:37 +0800

    添加ymlkey类

---
+++ code/APIServer/src/main/java/com/cc/pic/api/config/YmlKey.java
@@ -0,0 +1,12 @@
+package com.cc.pic.api.config;
+
+/**
+ * @ProJectName APIServer
+ * @FileName YmlKey
+ * @Description 系统yml配置文件中的key（此系统禁止使用properties问题）
+ * @Author CandyMuj
+ * @Date 2020/1/9 15:22
+ * @Version 1.0
+ */
+public class YmlKey {
+}
