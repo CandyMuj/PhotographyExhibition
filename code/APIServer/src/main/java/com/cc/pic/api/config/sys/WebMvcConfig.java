ec791298b038c148270fddca73ed3c74a208803b
MuJ <jx003js@gmail.com>
2020-01-15 16:00:50 +0800

    添加入参默认值处理规则

--- code/APIServer/src/main/java/com/cc/pic/api/config/sys/WebMvcConfig.java
+++ code/APIServer/src/main/java/com/cc/pic/api/config/sys/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.cc.pic.api.config.sys;
 
+import com.cc.pic.api.config.sys.c.DefaultArgumentResolver;
 import com.cc.pic.api.config.sys.c.TokenArgumentResolver;
 import com.cc.pic.api.intercept.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
@@ -23,12 +24,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Resource
     private TokenArgumentResolver tokenArgumentResolver;
     @Resource
+    private DefaultArgumentResolver defaultArgumentResolver;
+    @Resource
     private AuthInterceptor authInterceptor;
 
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(tokenArgumentResolver);
+        argumentResolvers.add(defaultArgumentResolver);
     }
 
     @Override
