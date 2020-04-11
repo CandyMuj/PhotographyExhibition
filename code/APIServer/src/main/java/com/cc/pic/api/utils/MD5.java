0e0313dc9fe8ee13fdad55426fd3eb2cd90826ff
MuJ <jx003js@gmail.com>
2020-04-11 14:16:18 +0800

    优化MD5工具

--- code/APIServer/src/main/java/com/cc/pic/api/utils/MD5.java
+++ code/APIServer/src/main/java/com/cc/pic/api/utils/MD5.java
@@ -14,7 +14,7 @@ public class MD5 {
 
     private static final String[] hexDigits = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
 
-    public MD5() {
+    private MD5() {
     }
 
     public static String byteArrayToHexString(byte[] b) {
