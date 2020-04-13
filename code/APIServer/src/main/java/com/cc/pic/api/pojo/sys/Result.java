5187cf9baff0063e535e8292bc044e914e356efc
MuJ <jx003js@gmail.com>
2020-04-13 22:14:18 +0800

    添加另一种回滚事务的方式，使用抛出异常的方式，spring提供的有便捷的抛出方法，此框架无法做到统一处理，具体看注释中的解释

--- code/APIServer/src/main/java/com/cc/pic/api/pojo/sys/Result.java
+++ code/APIServer/src/main/java/com/cc/pic/api/pojo/sys/Result.java
@@ -68,6 +68,10 @@ public class Result<T> {
 
     public static Result Error(String msg) {
         DB.setRollbackOnly();
+        // 另一种spring提供的抛出异常的方法，用来更方便的抛出异常，使事务可以捕获，执行回滚
+        // 但是在我这个框架里面不能用这种方式，此框架无法做到统一处理，因为在aop中也进行了error的返回那么就是死循环了（不停地进这个方法，不停地向上抛异常）
+        // Assert.isTrue(false, msg);
+        // throw new RuntimeException(msg);
         return new Result(FAIL, msg);
     }
 
