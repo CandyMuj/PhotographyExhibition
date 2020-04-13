2b26e4391ea52791d025f3f18ba4ffe781a1cddf
MuJ <jx003js@gmail.com>
2020-04-13 22:16:16 +0800

    添加了点注释

--- code/APIServer/src/main/java/com/cc/pic/api/pojo/sys/Result.java
+++ code/APIServer/src/main/java/com/cc/pic/api/pojo/sys/Result.java
@@ -70,6 +70,7 @@ public class Result<T> {
         DB.setRollbackOnly();
         // 另一种spring提供的抛出异常的方法，用来更方便的抛出异常，使事务可以捕获，执行回滚
         // 但是在我这个框架里面不能用这种方式，此框架无法做到统一处理，因为在aop中也进行了error的返回那么就是死循环了（不停地进这个方法，不停地向上抛异常）
+        // 如果用这种抛异常的方式，那么去掉方法体中的try catch catch (Exception e) { throw e;}继续向上抛，目的是让spring事务捕获这个异常
         // Assert.isTrue(false, msg);
         // throw new RuntimeException(msg);
         return new Result(FAIL, msg);
