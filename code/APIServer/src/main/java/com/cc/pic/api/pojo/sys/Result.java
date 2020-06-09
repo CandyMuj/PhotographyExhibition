413cdbd90c35c2933f1840bbe25ed76f39498a58
Muj <muj@llzey.com>
2020-06-09 11:49:18 +0800

    Result类扩展

--- code/APIServer/src/main/java/com/cc/pic/api/pojo/sys/Result.java
+++ code/APIServer/src/main/java/com/cc/pic/api/pojo/sys/Result.java
@@ -37,26 +37,25 @@ public class Result<T> {
     }
 
     public Result(T data, String msg) {
-        this.code = SUCCESS;
+        this(SUCCESS, data, msg);
+    }
+
+    public Result(int code, String msg) {
+        this(code, null, msg);
+    }
+
+    public Result(Throwable e) {
+        this(FAIL, null, e.getMessage());
+    }
+
+    public Result(int code, T data, String msg) {
+        this.code = code;
         this.data = data;
         this.msg = msg;
 
         printLog();
     }
 
-    public Result(int code, String msg) {
-        this.code = code;
-        this.msg = msg;
-
-        printLog();
-    }
-
-    public Result(Throwable e) {
-        this.code = FAIL;
-        this.msg = e.getMessage();
-
-        printLog();
-    }
 
     private void printLog() {
         if (StrUtil.isNotBlank(this.msg)) {
@@ -86,13 +85,17 @@ public class Result<T> {
     }
 
     public static Result Error(String msg) {
+        return Error(null, msg);
+    }
+
+    public static <T> Result Error(T data, String msg) {
         DB.setRollbackOnly();
         // 另一种spring提供的抛出异常的方法，用来更方便的抛出异常，使事务可以捕获，执行回滚
         // 但是在我这个框架里面不能用这种方式，此框架无法做到统一处理，因为在aop中也进行了error的返回那么就是死循环了（不停地进这个方法，不停地向上抛异常）
         // 如果用这种抛异常的方式，那么去掉方法体中的try catch catch (Exception e) { throw e;}继续向上抛，目的是让spring事务捕获这个异常
         // Assert.isTrue(false, msg);
         // throw new RuntimeException(msg);
-        return new Result(FAIL, msg);
+        return new Result(FAIL, data, msg);
     }
 
 }
