35ce00830e7e0e110ca3e5c0c82361a6e7b02695
Muj <muj@llzey.com>
2020-06-09 14:32:32 +0800

    业务方法中新增BaseController用作权限验证等操作

---
+++ code/APIServer/src/main/java/com/cc/pic/api/src/controller/BaseController.java
@@ -0,0 +1,14 @@
+package com.cc.pic.api.src.controller;
+
+/**
+ * @ProjectName api
+ * @FileName BaseController
+ * @Description
+ * @Author CandyMuj
+ * @Date 2020/6/9 14:30
+ * @Version 1.0
+ */
+public class BaseController {
+    
+
+}
