20e999f8cc60499b0801a687b66bb19ebee83ee6
Muj <muj@llzey.com>
2020-06-12 14:02:09 +0800

    提交，用户登录（未完成）

--- code/APIServer/src/main/java/com/cc/pic/api/src/controller/CustomerController.java
+++ code/APIServer/src/main/java/com/cc/pic/api/src/controller/CustomerController.java
@@ -1,9 +1,18 @@
 package com.cc.pic.api.src.controller;
 
+import com.cc.pic.api.pojo.sys.Result;
+import com.cc.pic.api.src.enumc.UserTypeEnum;
+import com.cc.pic.api.src.service.ICustomerService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @ProjectName PhotographyExhibition
@@ -16,7 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/customer")
-@Api(tags = "用户主表，登录信息")
+@Api(tags = "用户登录、注册")
 public class CustomerController {
+    @Resource
+    private ICustomerService customerService;
+
+
+    @ApiOperation(UserTypeEnum.ADMIN_CLIENT_STR + "登录")
+    @PostMapping("/admin/login")
+    public Result adminLogin(
+            @ApiParam(required = true, value = "账号") @RequestParam String account,
+            @ApiParam(required = true, value = "密码") @RequestParam String password
+    ) {
+        return null;
+    }
 
 }
