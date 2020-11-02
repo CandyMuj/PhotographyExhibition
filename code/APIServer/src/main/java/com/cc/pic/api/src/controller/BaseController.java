5c96346ee2b8e79838258b3579336a6635b40a0e
Candy <jx003js@gmail.com>
2020-11-02 15:16:49 +0800

    添加，系统日志管理；添加basecontroller验证账号

--- code/APIServer/src/main/java/com/cc/pic/api/src/controller/BaseController.java
+++ code/APIServer/src/main/java/com/cc/pic/api/src/controller/BaseController.java
@@ -1,6 +1,10 @@
 package com.cc.pic.api.src.controller;
 
+import com.cc.pic.api.enumc.Enable;
+import com.cc.pic.api.src.enumc.UserTypeEnum;
+import com.cc.pic.api.src.pojo.Customer;
 import com.cc.pic.api.src.service.ICustomerService;
+import com.cc.pic.api.src.service.ISystemLogService;
 
 import javax.annotation.Resource;
 
@@ -15,7 +19,30 @@ import javax.annotation.Resource;
 public class BaseController {
     @Resource
     protected ICustomerService customerService;
+    @Resource
+    protected ISystemLogService systemLogService;
 
 
+    /**
+     * 验证当前账号是否具有管理员操作权限
+     */
+    protected String validateAdmin(Customer customer) {
+        if (customer == null) {
+            return "账号不存在";
+        }
+        if (!this.isAdmin(customer)) {
+            return "无权操作";
+        }
+        if (!Enable.DISENABLE.getCode().equals(customer.getFrozen())) {
+            return "此账号被冻结";
+        }
+
+        return null;
+    }
+
+
+    private boolean isAdmin(Customer customer) {
+        return customer != null && UserTypeEnum.ADMIN.getType().equals(customer.getUserType());
+    }
 
 }
